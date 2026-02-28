3425eb258276b8d04619d2c99d55ec371d45acea
Sean Jordan <sean.e.jordan@gmail.com>
2026-02-28 16:30:47 -0600

    use placement array instead of start, tile list, direction

--- alphabet/engine.py
+++ alphabet/engine.py
@@ -1,6 +1,6 @@
-from typing import Self
+from typing import Self, List
 
-from alphabet.move import Move
+from alphabet.move import Move, Placement
 from alphabet.game import Game, Player
 from alphabet.board import Coord
 
@@ -12,9 +12,17 @@ class GameEngine:
     def select_move(self, game: Game, player: Player) -> Move:
 
         # for now, play all tiles at center, only move we know
+        current = Coord(row=7, col=7)
 
-        return Move(
-            start=game.board.at(Coord(row=7, col=7)),
-            tiles=list(player.tiles),
-            direction=Move.Direction.HORIZONTAL
-        )
+        placements: List[Placement] = []
+        for tile in player.tiles:
+            placements.append(
+                Placement(game.board.at(current), tile)
+            )
+            
+            current = Coord(
+                row=current.row,
+                col=current.col + 1
+            )
+
+        return Move(placements)
--- alphabet/move.py
+++ alphabet/move.py
@@ -2,47 +2,25 @@ from typing import List
 from enum import StrEnum
 
 from alphabet.player import Player
-from alphabet.board import Board, Tile, Square, Coord
+from alphabet.board import Board, Tile, Square
 
-#class Placement:
-#    def __init__(self, location: Square, tile: Tile):
+class Placement:
+    def __init__(self, location: Square, tile: Tile):
+        self.location = location
+        self.tile = tile
 
 class Move:
     Direction = StrEnum('Direction', ['HORIZONTAL', 'VERTICAL'])
 
-    def __init__(self, start: Square, tiles: List[Tile], direction: Direction):
-        self.start = start
-        self.tiles = tiles
-        self.direction = direction
-
-        # placements: List[Placement]
-        # self.placements = placements
+    def __init__(self, placements: List[Placement]):
+        self.placements = placements
 
     def score(self) -> int:
         return 5
     
     def play(self, player: Player, board: Board):
-        location = self.start.coord
-
-        print(self.tiles)
-        for tile in self.tiles:
-            print("play tile", tile)
-            player.play_letter(tile)
-
-            board.place_tile(tile, coord=location)
-
-            # find the next open slot in the direction we're moving
-            while board.is_occupied(location):
-                rowmod = 1 if self.direction == Move.Direction.VERTICAL else 0
-                colmod = 1 if rowmod == 0 else 0
-
-                location = Coord(
-                    row=location.row + rowmod,
-                    col=location.col + colmod
-                )
-
-                # stop if reach out of bounds
-                assert board.is_in_bounds(location), "Illegal move, extends beyond board's boundary!"
-
+        for placement in self.placements:
+            player.play_letter(placement.tile)
+            board.place_tile(placement.tile, coord=placement.location.coord)
         player.add_points(self.score())
 
